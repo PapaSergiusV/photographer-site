5808d1cb4d1ccff79f70ce3c67ca24aa4d2c772e
Сергей Пономарев <31237530+PapaSergiusV@users.noreply.github.com>
2018-08-21 12:16:35 +0300

    v 0.5

---
+++ PhotographerWebsite/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotographerWebsite.Models
+{
+    public class Email
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Text { get; set; }
+    }
+}
