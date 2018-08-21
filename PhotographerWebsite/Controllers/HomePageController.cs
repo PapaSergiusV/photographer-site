d73f4745aa19e957b4f726e3cd54ffb30a1ecfdb
Sergei Ponomarev <ponomar.sergey@yandex.ru>
2018-08-21 15:26:51 +0300

    v 0.5.1

--- PhotographerWebsite/Controllers/HomePageController.cs
+++ PhotographerWebsite/Controllers/HomePageController.cs
@@ -4,19 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PhotographerWebsite.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace PhotographerWebsite.Controllers
 {
     public class HomePageController : Controller
     {
+        private readonly IHostingEnvironment _env;
+
+        public HomePageController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public string EmailHandler(Email email)
+        public FileResult EmailHandler(Email email)
         {
-            return $"Name: {email.Name};\nPhone: {email.Phone};\nText: {email.Text}";
+            string name = DateTime.Now.Millisecond + ".txt";
+            string path = _env.ContentRootPath + "/" + name;
+            using (var sw = new StreamWriter(new FileStream(path, FileMode.OpenOrCreate)))
+            {
+                sw.WriteLine($"Name: {email.Name}");
+                sw.WriteLine($"Phone: {email.Phone}");
+                sw.WriteLine($"Text: {email.Text}");
+                sw.WriteLine(DateTime.Now);
+            }
+            return File(new FileStream(path, FileMode.Open), "Application/txt", name);
         }
     }
 }
